3cdeade0fb259bf4cf2a367ada355f7d0f940bc4
Furkan Tokkan <efurk64@gmail.com>
2023-07-10 04:06:12 +0300

    Validate user input

--- Assets/Scripts/LoginUI.cs
+++ Assets/Scripts/LoginUI.cs
@@ -1,29 +1,71 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LoginUI : MonoBehaviour
 {
-    private Button loginButton;
-    private Button sendButton;
+    [Header("Settings")]
+    [SerializeField] private int maxUsernameLength = 10;
+    [SerializeField] private int maxPasswordLength = 10;
+    [Space]
+    [Header("Referances")]
+    [SerializeField] private Button loginButton;
+    [SerializeField] private TextMeshProUGUI loginText;
+    [SerializeField] private TMP_InputField usernameInputfield;
+    [SerializeField] private TMP_InputField passwordInputfield;
 
-    private void Awake()
-    {
-        loginButton = transform.Find("Connect").GetComponent<Button>();
-        loginButton.onClick.AddListener(Connect);
+    private string username = string.Empty;
 
-        sendButton = transform.Find("Send").GetComponent<Button>();
-        sendButton.onClick.AddListener(Send);
-    }
-    private void Send()
+    private string password = string.Empty;
+
+
+    private void Start()
     {
-        NetworkClient.Instance.SendServer("Test Message!");
+        loginButton.onClick.AddListener(Login);
+
+        usernameInputfield.onValueChanged.AddListener(UpdateUsername);
+
+        passwordInputfield.onValueChanged.AddListener(UpdatePassword);
     }
-    private void Connect()
+
+    private void UpdateUsername(string value)
     {
-        NetworkClient.Instance.Connect();
-        Debug.Log("Connecting...");
+        username = value;
+        ValidateUserInput();
+    }
+
+    private void UpdatePassword(string value)
+    {
+        password = value;
+        ValidateUserInput();
+    }
+
+    private void ValidateUserInput()
+    {
+        var usernameRegex = Regex.Match(username, "^[a-zA-Z0-9]+$");
+
+        bool interactable = !string.IsNullOrWhiteSpace(username) &&
+            !string.IsNullOrWhiteSpace(password) &&
+            username.Length <= maxUsernameLength &&
+            password.Length <= maxPasswordLength &&
+            usernameRegex.Success;
+
+        InteractLoginButton(interactable);
+    }
+
+    private void InteractLoginButton(bool interactable)
+    {
+        loginButton.interactable = interactable;
+        Color color = loginButton.interactable ? Color.white : Color.grey;
+        loginText.color = color;
+    }
+
+    private void Login()
+    {
+        Debug.Log("Logining in!");
     }
 }
